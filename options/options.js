937dab44e90c41d6949f21657d378a538663ab8d
Crowdbotics <team@crowdbotics.com>
2023-04-08 02:23:56 +0000

    Initial commit.

---
+++ options/options.js
@@ -0,0 +1,12 @@
+import { StyleSheet } from "react-native"
+
+export const globalOptions = {
+  name: "bold-smoke-39784",
+  url: "https://bold-smoke-39784.botics.co",
+  api: "https://bold-smoke-39784.botics.co/api/v1"
+}
+export const modulesOptions = {
+  "@modules/app-menu": {
+    copy: "Routes available!"
+  }
+}
